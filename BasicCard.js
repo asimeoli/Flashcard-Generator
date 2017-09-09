573ef543d5f788d4125616c45c208f5b90b6cbd8
asimeoli <asimeoli@gmail.com>
2017-09-09 10:42:42 -0400

    Update BasicCard.js

--- BasicCard.js
+++ BasicCard.js
@@ -4,7 +4,6 @@
 // The constructor should accept two arguments: front and back.
 // The constructed object should have a front property that contains the text on the front of the card.
 // The constructed object should have a back property that contains the text on the back of the card.
-var fs = require("fs");
 var inquirer = require("inquirer");
 
 var BasicCard = function(front, back) {
